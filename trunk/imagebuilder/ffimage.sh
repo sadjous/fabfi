b42fadd5a23150bea104f04672f7408918b4148f
berkoben <berkoben@c75bf3ec-ef49-773c-e2a6-a9548360da88>
2011-01-21 15:10:30 +0000

    corrected errors in imagebuilder scripts.  Now all .mk files have FILES:=fabfi only

--- trunk/imagebuilder/ffimage.sh
+++ trunk/imagebuilder/ffimage.sh
@@ -7,7 +7,7 @@
 echo "pick a profile (and you'd better spell it right or this thing blows up)"
 read profile
 
-here=$(pwd) #home/fabfi/imagebuilder/
+here=$(pwd) #svn/fabfi/trunk/imagebuilder/
 
 #
 echo "enter path to the image builder (no trailing /)"
@@ -27,7 +27,8 @@ ln -s $IBMake ${IBPATH}/${IBDIR}/target/linux/ar71xx/image/Makefile
 cd ../
 
 cp -a $(pwd)/files/router_configs/${profile} ${IBPATH}/${IBDIR}/fabfi/
-cp -a $(pwd)/files/router_configs/common/* ${IBPATH}/${IBDIR}/fabfi/${profile}
+#cp -a $(pwd)/files/router_configs/common/* ${IBPATH}/${IBDIR}/fabfi/${profile}
+cp -a $(pwd)/files/router_configs/common/* ${IBPATH}/${IBDIR}/fabfi/
 
 cd ${IBPATH}/${IBDIR}
 make image PROFILE=${profile}
