b53573725c2e09780b8022814716d11e58ed755c
berkoben <berkoben@c75bf3ec-ef49-773c-e2a6-a9548360da88>
2011-08-22 14:25:09 +0000

    tagged trunk for (potentially) last-ever 4.0 build

---
+++ tags/4.0.0_RC3/imagebuilder/target/linux/ar71xx/profiles/UBNTNANOM/ubnt.mk
@@ -0,0 +1,87 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/UBNTNANOM
+  NAME:=ubiquiti NanostationM
+  FILES:=fabfi
+## Assuming the following are automatically added
+# base-files busybox dnsmasq dropbear firewall hotplug2 iptables kmod-diag kmod-ipt-nathelper kmod-switch
+# libc libgcc mtd nvram opkg ppp ppp-mod-pppoe uci udevtrigger wpad-mini 
+#
+## Order of Package files
+# USB
+# Wireless
+# Firewall stuff
+# Fabfi crap...
+
+  PACKAGES:=kmod-ath kmod-ath9k kmod-mac80211 -kmod-madwifi libnl-tiny crda wpad-mini \
+libuci bridge \
+olsrd olsrd-mod-nameservice olsrd-mod-txtinfo olsrd-mod-dyn-gw \
+awesome-chilli kmod-tun ip \
+mini-httpd netcat \
+libpcap tcpdump \
+libopenssl libpthread \
+uclibcxx iperf \
+snmpd \
+libmcrypt libwrap vnstat vnstati iftop libjpeg libgd libpng \
+snmp-utils \
+libnetsnmp rrdtool libart \
+libfreetype librrd \
+kmod-gpio-dev kmod-button-hotplug kmod-input-core kmod-input-gpio-buttons kmod-input-polldev kmod-leds-gpio \
+-kmod-usb-core -kmod-usb-ohci -kmod-usb2 -kmod-scsi-core -kmod-usb-storage -kmod-fs-ext3 -e2fsprogs -block-extroot -block-mount -kmod-fs-mbcache \
+-ppp -ppp-mod-pppoe -kmod-ppp -kmod-pppoe -kmod-crc-ccitt -uhttpd \
+-squid
+
+
+#libpcap tcpdump \ uclibcxx iperf \ gpioctl \
+
+#kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-scsi-core kmod-usb-storage block-extroot \
+#	crda iw kmod-b43 kmod-b43legacy kmod-cfg80211 kmod-mac80211 wireless-tools libnl-tiny \
+#	libiptc libxtables kmod-ipt-conntrack kmod-ipt-core iptables-mod-conntrack iptables-mod-nat kmod-ipt-nat luci \
+	
+
+
+endef
+
+define Profile/UBNTNANOM/Description
+	Package set optimized for the NANO and PICO M series Fabfis
+endef
+$(eval $(call Profile,UBNTNANOM))
+
+
+define Profile/UBNTRS
+	NAME:=Ubiquiti RouterStation
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2
+endef
+
+define Profile/UBNTRS/Description
+	Package set optimized for the Ubiquiti RouterStation.
+endef
+
+$(eval $(call Profile,UBNTRS))
+
+define Profile/UBNTRSPRO
+	NAME:=Ubiquiti RouterStation Pro
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2
+endef
+
+define Profile/UBNTRSPRO/Description
+	Package set optimized for the Ubiquiti RouterStation Pro.
+endef
+
+$(eval $(call Profile,UBNTRSPRO))
+
+define Profile/UBNT
+	NAME:=Ubiquiti Products
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2
+endef
+
+define Profile/UBNT/Description
+	Build images for all Ubiquiti products (including LS-SR71, RouterStation and RouterStation Pro)
+endef
+
+$(eval $(call Profile,UBNT))
---
+++ tags/4.0.0_RC3/imagebuilder/target/linux/ar71xx/profiles/WRT160NL/linksys.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/WRT160NL
+	NAME:=Linksys WRT160NL
+	FILES:=fabfi
+	PACKAGES:=kmod-ath9k kmod-mac80211 wpad-mini kmod-usb-core -kmod-madwifi kmod-usb2 swconfig \
+libuci bridge \
+kmod-scsi-core kmod-usb-storage kmod-fs-ext3 e2fsprogs block-extroot fdisk block-mount \
+kmod-gpio-dev kmod-input-core kmod-input-gpio-buttons kmod-input-polldev kmod-leds-gpio kmod-usb-serial \
+kmod-usb-serial-option kmod-usb-uhci \
+kmod-usb-ohci kmod-nls-base kmod-scsi-generic   \
+olsrd olsrd-mod-nameservice olsrd-mod-txtinfo olsrd-mod-dyn-gw \
+mini-httpd netcat \
+kmod-tun ip \
+libopenssl libpthread swap-utils squid \
+snmpd \
+uclibcxx iperf \
+libpcap tcpdump \
+awesome-chilli libjson \
+libmcrypt libwrap vnstat vnstati iftop libjpeg libgd libpng \
+sdparm comgt chat  usbutils zlib libusb usb-modeswitch usb-modeswitch-data \
+ppp ppp-mod-pppoe kmod-ppp kmod-pppoe kmod-crc-ccitt qos-scripts snmp-utils \
+libnetsnmp collectd collectd-mod-network collectd-mod-rrdtool rrdtool libart \
+libfreetype librrd 
+
+
+endef
+
+define Profile/WRT160NL/Description
+	Package set optimized for the Linksys WRT160NL.
+endef
+
+define Profile/WRT400N
+	NAME:=Linksys WRT400N
+	PACKAGES:=kmod-ath9k wpad-mini
+endef
+
+define Profile/WRT400N/Description
+	Package set optimized for the Linksys WRT400N.
+endef
+
+$(eval $(call Profile,WRT160NL))
+$(eval $(call Profile,WRT400N))
